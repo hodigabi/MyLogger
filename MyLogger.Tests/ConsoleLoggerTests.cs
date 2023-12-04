62e89faf0c9ec3e34502ce4bd210fd2fcc813ca5
Gabor Hodi <gabor.hodi@betssongroup.com>
2023-12-04 22:43:43 +0100

    Add files to git

---
+++ MyLogger.Tests/ConsoleLoggerTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Extensions.Logging;
+using MyLogger.Exceptions;
+using MyLogger.Loggers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace MyLogger.Tests
+{
+    public class ConsoleLoggerTests
+    {
+        private readonly ConsoleLogger _consoleLogger;
+
+        private Dictionary<LogLevel, ConsoleColor> LogLevelToColorMap { get; set; } = new Dictionary<LogLevel, ConsoleColor>()
+        {
+            [LogLevel.Error] = ConsoleColor.Red,
+            [LogLevel.Information] = ConsoleColor.Green,
+            [LogLevel.Debug] = ConsoleColor.Gray,
+        };
+
+        public ConsoleLoggerTests()
+        {
+            _consoleLogger = new ConsoleLogger(Encoding.UTF8);
+        }
+
+        [Fact]
+        public void LogMessage_ShouldThrow_WhenTooLogMessage()
+        {
+            // Arrange
+            var longString = string.Concat(Enumerable.Repeat("XY", 501));
+
+            // Act & Assert
+            Assert.Throws<MessageLengthException>(() => _consoleLogger.Log(LogLevel.Debug, longString));
+        }
+
+        [Theory]
+        [InlineData(LogLevel.Error)]
+        [InlineData(LogLevel.Information)]
+        [InlineData(LogLevel.Debug)]
+        public void LogMessage_ConsoleForegroundIsSet_BasedOnLogLevel(LogLevel logLevel)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            _consoleLogger.Log(logLevel, "Should be colored");
+
+            Assert.Equal(LogLevelToColorMap[logLevel], Console.ForegroundColor);
+        }
+
+        [Fact]
+        // This tests the base class' functionality
+        public void LogMessage_Format()
+        {
+            // #{LogTime} [#{LogLevel}] #{LogMessage}
+
+            // Arrange
+            LogLevel logLevel = LogLevel.Error;
+            var logMessage = "This is a log message";
+
+            var expectedLogMessage = $"{DateTime.UtcNow} [{logLevel}] {logMessage}{Environment.NewLine}";
+
+            var consoleWriter = new StringWriter();
+            Console.SetOut(consoleWriter);
+
+            // Act
+            _consoleLogger.Log(logLevel, logMessage);
+
+            // Assert
+            Assert.Equal(expectedLogMessage, consoleWriter.ToString());
+        }
+    }
+}
---
+++ MyLogger.Tests/FileLoggerTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.Logging;
+using MyLogger.Loggers;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace MyLogger.Tests
+{
+    public class FileLoggerTests
+    {
+        private string filePath1 = "C:\\test1\\testlog.txt";
+        private string filePath2 = "C:\\test2\\testlog.txt";
+
+        /// <summary>
+        /// The unit tests of this class are testing the basic functionality
+        /// however, the file system should have been mocked
+        /// </summary>
+        public FileLoggerTests()
+        {
+            CleanFiles(filePath1);
+            CleanFiles(filePath2);
+        }
+
+        [Fact]
+        public void ContructArchivePath_ShouldIndexFileName()
+        {
+            var fileLogger = new FileLogger(Encoding.UTF8, filePath1);
+
+            //#{LogFileName}.#NextNumber.#{LogFileExtension}
+            var expectedName1 = "C:\\test1\\testlog.1.txt";
+
+            // yes, run 2 should result the same result because
+            // no actual testlog.1.txt is created
+            var expectedName2 = "C:\\test1\\testlog.1.txt";
+
+            // Act
+            var archiveName1 = fileLogger.ConstructArchivePath();
+            var archiveName2 = fileLogger.ConstructArchivePath();
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.Equal(expectedName1, archiveName1);
+                Assert.Equal(expectedName2, archiveName2);
+            });
+        }
+
+        [Fact(Skip = "Runner/environment dependent")]
+        public void LotOfLogs_RotateFile()
+        {
+            // Arrange
+            var fileLogger = new FileLogger(Encoding.UTF8, filePath2);
+            var longLogMessage = string.Concat(Enumerable.Repeat("AA", 5100));
+
+            // Act
+            fileLogger.Log(LogLevel.Information, longLogMessage);
+            fileLogger.Log(LogLevel.Information, ".");
+
+            // Assert
+            Assert.True(File.Exists("C:\\test2\\testlog.1.txt"));
+        }
+
+        private void CleanFiles(string path)
+        {
+            path = Path.GetDirectoryName(path);
+
+            DirectoryInfo directoryInfo = new(path);
+
+            if (directoryInfo.Exists)
+            {
+                foreach (FileInfo file in directoryInfo.GetFiles())
+                {
+                    file.Delete();
+                }
+            }
+        }
+    }
+}
